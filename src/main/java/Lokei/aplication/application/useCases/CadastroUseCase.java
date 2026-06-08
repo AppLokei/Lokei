73c6530a059d0e8544d0e274e1a3ac406001dc15
Yasmin <yasminrocha1300@gamil.com>
2026-06-08 20:15:43 -0300

    criptografia da senha em BCrypt

--- src/main/java/Lokei/aplication/application/useCases/CadastroUseCase.java
+++ src/main/java/Lokei/aplication/application/useCases/CadastroUseCase.java
@@ -4,6 +4,7 @@ import Lokei.aplication.infrastructure.exception.UsuarioException;
 import Lokei.aplication.infrastructure.persistence.entity.Usuario;
 import Lokei.aplication.infrastructure.persistence.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +12,9 @@ public class CadastroUseCase {
 
     @Autowired
     private UsuarioRepository usuarioRepo;
+
+    @Autowired
+    private PasswordEncoder passEncoder;
 
     public String cadastro(Usuario usuario){
 
@@ -20,6 +24,10 @@ public class CadastroUseCase {
         if(cpfJaCadastrado) throw new UsuarioException("Este CPF já está associado a uma conta");
 
         usuario.validacaoDadosUsuario();
+
+        usuario.setSenha(
+                passEncoder.encode(usuario.getSenha()));
+
         usuarioRepo.save(usuario);
         return "Cadastro concluído com sucesso!";
     }
